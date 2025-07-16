2e02bb067aef6fa518a3b9be0a70c1f99c27209d
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-07-16 11:10:58 +0800

    Split into sections (#10)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
-  <div id="app">
+  <div>
+  <HeaderNavBar></HeaderNavBar>
+  <main class="main">
+    <HeroSection></HeroSection>
+    <WhatWeDoSection></WhatWeDoSection>
+    <AboutSection></AboutSection>
+    <SkillsSection></SkillsSection>
+    <StatsSection></StatsSection>
+    <ServicesSection></ServicesSection>
+    <PortfolioSection></PortfolioSection>
+    <TestimonialsSection></TestimonialsSection>
+    <TeamSection></TeamSection>
+    <ContactSection></ContactSection>
     
+  </main>
+  <FooterNavMenu></FooterNavMenu>
   </div>
 </template>
 
 <script>
+import HeaderNavBar from './components/HeaderNavBar.vue';
+import FooterNavMenu from './components/FooterNavMenu.vue';
+
+import HeroSection from './components/HeroSection.vue';
+import WhatWeDoSection from './components/WhatWeDoSection.vue';
+import AboutSection from './components/AboutSection.vue';
+import SkillsSection from './components/SkillsSection.vue';
+import StatsSection from './components/StatsSection.vue';
+import ServicesSection from './components/ServicesSection.vue';
+import PortfolioSection from './components/PortfolioSection.vue';
+import TestimonialsSection from './components/TestimonialsSection.vue';
+import TeamSection from './components/TeamSection.vue';
+import ContactSection from './components/ContactSection.vue';
+
 export default {
   name: 'App',
   components: {
-    
+    HeaderNavBar,
+    FooterNavMenu,
+    HeroSection,
+    WhatWeDoSection,
+    AboutSection,
+    SkillsSection,
+    StatsSection,
+    ServicesSection,
+    PortfolioSection,
+    TestimonialsSection,
+    TeamSection,
+    ContactSection
   }
 }
 </script>
-
-
