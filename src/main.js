f8a57d7f3b313a010a557bdfa3860d6fc6e55cde
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-07-24 08:28:30 +0800

    Update ScrollTopButton to Vue custom directive, prop, data, class and event binding

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import './assets/css/main.css'
 
 Vue.config.productionTip = false
 
+// Reference: https://v2.vuejs.org/v2/cookbook/creating-custom-scroll-directives.html
+// To capture window scroll event.
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        document.removeEventListener('scroll', f)
+      }
+    }
+    document.addEventListener('scroll', f)
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
