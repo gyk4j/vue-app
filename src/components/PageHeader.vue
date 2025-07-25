1452a387e8f023ad8694f7f2303ea002dc83c5b4
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-07-25 08:51:08 +0800

    Move header menu toggle-dropdown

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -74,18 +74,6 @@ export default {
       });
 
     });
-
-    /**
-     * Toggle mobile nav dropdowns
-     */
-    document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-      navmenu.addEventListener('click', function(e) {
-        e.preventDefault();
-        this.parentNode.classList.toggle('active');
-        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-        e.stopImmediatePropagation();
-      });
-    });
   },
   methods: {
     mobileNavToogle() {
