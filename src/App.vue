d0e1853e3a0515bb2d0aa1d236cc362efcbf872b
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-08-04 15:18:38 +0800

    Move testimonials data

--- src/App.vue
+++ src/App.vue
@@ -323,6 +323,53 @@ export default {
           },
         ],
       },
+      testimonials: {
+        title: 'Testimonials',
+        subtitle: 'Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit',
+        testimonials:
+        [
+          {
+            id: 0,
+            img: 'assets/img/testimonials/testimonials-1.jpg',
+            name: 'Saul Goodman',
+            role: 'Ceo & Founder',
+            rating: 5,
+            quote: 'Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.',
+          },
+          {
+            id: 1,
+            img: 'assets/img/testimonials/testimonials-2.jpg',
+            name: 'Sara Wilsson',
+            role: 'Designer',
+            rating: 4,
+            quote: 'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.',
+          },
+          {
+            id: 2,
+            img: 'assets/img/testimonials/testimonials-3.jpg',
+            name: 'Jena Karlis',
+            role: 'Store Owner',
+            rating: 3,
+            quote: 'Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.',
+          },
+          {
+            id: 3,
+            img: 'assets/img/testimonials/testimonials-4.jpg',
+            name: 'Matt Brandon',
+            role: 'Freelancer',
+            rating: 2,
+            quote: 'Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.',
+          },
+          {
+            id: 4,
+            img: 'assets/img/testimonials/testimonials-5.jpg',
+            name: 'John Larson',
+            role: 'Entrepreneur',
+            rating: 1,
+            quote: 'Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.',
+          },
+        ]
+      }
     }
   },
   computed: {
@@ -335,6 +382,7 @@ export default {
         stats: this.stats,
         services: this.services,
         portfolio: this.portfolio,
+        testimonials: this.testimonials,
       }
     }
   },
