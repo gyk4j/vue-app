ac52e3da07ec7cf44d5ffde2391f374af8ef624a
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-08-07 22:20:02 +0800

    Fix duplicate keys

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -44,8 +44,8 @@
               <h3>{{ t.name }}</h3>
               <h4>{{ t.role }}</h4>
               <div class="stars">
-                <i v-for="(r, idx) in t.rating" v-bind:key="idx" class="bi bi-star-fill"></i>
-                <i v-for="(r, idx) in 5-t.rating" v-bind:key="idx" class="bi bi-star"></i>
+                <i v-for="(r, idx) in t.rating" v-bind:key="'f'+idx" class="bi bi-star-fill"></i>
+                <i v-for="(r, idx) in 5-t.rating" v-bind:key="'e'+idx" class="bi bi-star"></i>
               </div>
               <p>
                 <i class="bi bi-quote quote-icon-left"></i>
