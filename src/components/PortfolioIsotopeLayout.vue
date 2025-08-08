3e0db810e6bde1fc55f1939225578af77b7301fe
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-08-08 15:32:07 +0800

    Update DOM-styled state management to Vue-style

--- src/components/PortfolioIsotopeLayout.vue
+++ src/components/PortfolioIsotopeLayout.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+  <div
+    class="isotope-layout"
+    v-bind:data-default-filter="filter"
+    v-bind:data-layout="layout"
+    v-bind:data-sort="sort">
 
     <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
-      <template v-for="f in value.filters">
-        <li v-bind:key="f.id" v-bind:data-filter="f.filter" v-bind:class="{ 'filter-active': f.filter==='*' }">{{ f.text }}</li>
-      </template>
+      <li
+        v-for="f in value.filters" 
+        v-bind:key="f.id" 
+        v-bind:data-filter="f.filter" 
+        v-bind:class="{ 'filter-active': f.filter===filter }"
+        v-on:click="onClickFilter(f)">
+        {{ f.text }}
+      </li>
     </ul><!-- End Portfolio Filters -->
 
-    <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+    <div class="row gy-4 isotope-container" ref="isotopeContainer" data-aos="fade-up" data-aos-delay="200">
 
       <PortfolioIsotopeItem v-for="project in value.projects" v-bind:key="project.id" v-bind:value="project"></PortfolioIsotopeItem>
 
@@ -23,6 +32,14 @@ export default {
   name: 'PortfolioIsotopeLayout',
   components: {
     PortfolioIsotopeItem
+  },
+  data() {
+    return {
+      layout: 'masonry',
+      filter: '*',
+      sort: 'original-order',
+      initIsotope: null,
+    }
   },
   props: {
     value: {
@@ -43,38 +60,29 @@ export default {
     /**
      * Init isotope layout and filters
      */
-    document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
-      let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
-      let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
-      let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
-
-      let initIsotope;
+    // eslint-disable-next-line
+    imagesLoaded(this.$refs.isotopeContainer, this.onImagesLoaded);
+  },
+  methods: {
+    onImagesLoaded: function() {
       // eslint-disable-next-line
-      imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
-        // eslint-disable-next-line
-        initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-          itemSelector: '.isotope-item',
-          layoutMode: layout,
-          filter: filter,
-          sortBy: sort
-        });
+      this.initIsotope = new Isotope(this.$refs.isotopeContainer, {
+        itemSelector: '.isotope-item',
+        layoutMode: this.layout,
+        filter: this.filter,
+        sortBy: this.sort
       });
-
-      // eslint-disable-next-line
-      isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-        filters.addEventListener('click', function() {
-          isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-          this.classList.add('filter-active');
-          initIsotope.arrange({
-            filter: this.getAttribute('data-filter')
-          });
-          if (typeof aosInit === 'function') {
-            this.aosInit();
-          }
-        }, false);
+    },
+    onClickFilter: function(filter) {
+      this.filter = filter.filter
+      
+      this.initIsotope.arrange({
+        filter: this.filter
       });
-
-    });
+      if (typeof aosInit === 'function') {
+        this.aosInit();
+      }
+    }
   }
 }
 </script>
