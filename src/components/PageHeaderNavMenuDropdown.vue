9773c816cad1775f533d467cccb3278906221769
gyk4j <147011991+gyk4j@users.noreply.github.com>
2025-08-11 10:20:00 +0800

    Add header nav menu dropdown sub-menu (#52)

---
+++ src/components/PageHeaderNavMenuDropdown.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="dropdown">
+    <a href="#" v-bind:class="{ 'active': active }" v-on:click="toggleMobileNav">
+      <span v-bind:class="{ 'dropdown-active': dropdownActive }">{{ text }}</span>
+      <i class="bi bi-chevron-down toggle-dropdown" v-on:click="toggleDropDown"></i>
+    </a>
+    <ul>
+      <slot></slot>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'PageHeaderNavMenuDropdown',
+  props: {
+    active: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    text: {
+      type: String,
+      required: false,
+      default: ''
+    },    
+  },
+  data() {
+    return {
+      dropdownActive: false
+    }
+  },
+  methods: {
+    toggleDropDown(/* e */) {
+      // e.preventDefault();      
+      this.dropdownActive = !this.dropdownActive;
+      // e.stopImmediatePropagation();
+    },
+    toggleMobileNav() {
+      this.$emit('toggle-mobile-nav')
+    }
+  }
+}
+</script>
